a04ab860794881611cbda742f4355e152e503ce1
Omar <omar.abdullah399@web.de>
2025-10-29 15:58:04 +0100

    Ändere die Exam-Klasse, um die Modulvererbung zu entfernen und die Modul-Instanz als Attribut hinzuzufügen

--- models/exam.py
+++ models/exam.py
@@ -3,9 +3,11 @@ from dataclasses import dataclass
 from .module import Module
 
 @dataclass
-class Exam(Module):
+class Exam:
+    module: Module
     attempt: int = 1
-
+    grade: float | None = None
+    
     def retake(self):
         """Neue Prüfung schreiben"""
         self.attempt += 1
